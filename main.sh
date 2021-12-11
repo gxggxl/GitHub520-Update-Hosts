f261829f1ad055c553ff6d95be63e8f919847b2d
gxggxl <gxggxl@qq.com>
2021-12-11 09:37:55 +0800

    HOST_URL Update
    - 改用实时 Host Url
    - 文件资源使用代理（https://ghproxy.com/）加速

--- main.sh
+++ main.sh
@@ -7,25 +7,28 @@
 # @Project Name : GitHub520-Update-Hosts
 # @Description  : 脚本功能更新hosts文件
 
-# 当前目录测试 0 是生产坏境
-debug=0
+# 当前目录测试为 1 (0 是生产坏境)
+Debug=0
 # 资源配置
 sysPath="/etc"
-hostUrl="https://cdn.jsdelivr.net/gh/521xueweihan/GitHub520@main/hosts"
+# 由于github仓库拉取较慢，所以会默认添加代理前缀，如不需要请移除
+GithubProxyUrl="https://ghproxy.com/"
+# 资源URL
+HostUrl="https://cdn.jsdelivr.net/gh/521xueweihan/GitHub520@main/hosts"
+HostUrl="${GithubProxyUrl}https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts"
 
-if ((debug == 1)); then
+if ((Debug == 1)); then
   sysPath="../etc"
   mkdir -p $sysPath
 else
-  wwww=$sysPath
-  sysPath=$wwww
+  echo -e "你的系统目录为：${sysPath}"
 fi
 # 更新系统 hosts 文件
 function updateHost() {
   echo "正在删除旧的 GitHub520Host 文件......"
   cat <"$sysPath"/hosts | sed '/^# GitHub520 Host Start/,/^# GitHub520 Host End/d' >tmpfile && mv tmpfile "$sysPath"/hosts
   echo "正在下载新的 GitHub520Host 文件......"
-  curl -L "$hostUrl" >>"$sysPath"/hosts
+  curl -L "$HostUrl" >>"$sysPath"/hosts
   cat "$sysPath"/hosts
   echo "更新完成！！！！！！"
 }
